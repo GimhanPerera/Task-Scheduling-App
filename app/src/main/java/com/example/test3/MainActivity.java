1e346e006b90f889b4a2fbc8eb82116419aeafde
Gimhan1 <rashmithagimhan32@gmail.com>
2023-10-19 20:39:30 +0530

    Navigation v1.0 added

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.test3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.*;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    NotificationFragment notificationFragment = new NotificationFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem item1 = menu.findItem(R.id.Home);
+        MenuItem item2 = menu.findItem(R.id.Notification);
+        MenuItem item3 = menu.findItem(R.id.Profile);
+
+
+        //item1.setTitle(item2.getTitle());
+        //String s= item1.getItemId();
+        //bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if(item.getTitle()==item1.getTitle()){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                }
+                else if(item.getTitle()==item2.getTitle()){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                }
+                else if(item.getTitle()==item3.getTitle()){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                }
+
+
+                /*switch (item.getItemId()){
+                    case R.id.Home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        break;
+                    case R.id.Notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                        break;
+                    case R.id.Profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        break;
+                }*/
+
+                return false;
+            }
+        });
     }
 }
