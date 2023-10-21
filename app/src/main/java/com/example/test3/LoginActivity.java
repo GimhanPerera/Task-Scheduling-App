9dc5f9bda7a73a3a6cbae4f274ab9e51e4b1ad61
Gimhan1 <rashmithagimhan32@gmail.com>
2023-10-21 15:37:27 +0530

    Login page added to the flow

--- app/src/main/java/com/example/test3/LoginActivity.java
+++ app/src/main/java/com/example/test3/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btn_logIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_logIn=findViewById(R.id.btn_login);
+        btn_logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLogin();
+            }
+        });
+    }
+
+    public void btnLogin(){
+        Intent intent=new Intent(this,HomeActivity.class);
+        startActivity(intent);
     }
 }
