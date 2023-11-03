ff9bcebb812c56dd1dbc731026388d7a17e76011
Gimhan1 <rashmithagimhan32@gmail.com>
2023-11-03 10:45:54 +0530

    Home page bug fixing s2.0 SignUpActivity updated

--- app/src/main/java/com/example/test3/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/test3/Adapter/ToDoAdapter.java
@@ -102,34 +102,24 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     task.setCompletion(isChecked);
+
+                //--------------------------------------
                     if(isChecked){
                         task.setCompletion(true);
                         holder.todoTitle.setPaintFlags(holder.todoTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                        /*--------------------------------------
-                        try{
+                        /*try{
                             listClass object=listClass.getInstance();
-                            Task temp = object.getTaskList().get(position);
-                            object.getTaskList().remove(position); //Remove the item
-                            reArrange();//Rearrange
-                            Snackbar snackbar = Snackbar.make(relLayout, "Item deleted", Snackbar.LENGTH_SHORT)
-                                    .setAction("UNDO", new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            //Task temp = new Task();
-                                            //int nexid = object.getNextId();
-                                            object.setNewTask(temp);
-                                            reArrange();
-                                            Toast.makeText(requireContext(),"DONE: "+temp.getTitle(), Toast.LENGTH_SHORT).show();//For Testing
-                                        }
-                                    });
-                            snackbar.show();
+                            int index= object.getTaskList().indexOf(task);
+                            Task temp = object.getTaskList().get(index);
+                            object.getTaskList().remove(index); //Remove the item
+                            //HomeFragment homeFragment = new HomeFragment();
+                            //homeFragment.reArrange();//Rearrange
                         }catch (Exception ex){
-                            Toast.makeText(requireContext(),"Exception!!", Toast.LENGTH_SHORT).show();
-                        }
-                        //--------------------------------------*/
+                            //Toast.makeText(requireContext(),"Exception!!", Toast.LENGTH_SHORT).show();
+                        }*/
                     }
                     else{
-                        task.setCompletion(true);
+                        task.setCompletion(false);
                         holder.todoTitle.setPaintFlags(holder.todoTitle.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
                     }
 
@@ -158,4 +148,17 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> 
             parentLayout = itemView.findViewById(R.id.oneLineTaskLayout);
         }
     }
+    Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                // Sleep for 2000 milliseconds (2 seconds)
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Code to be executed after the delay
+        }
+    });
 }
--- app/src/main/java/com/example/test3/SignUpActivity.java
+++ app/src/main/java/com/example/test3/SignUpActivity.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 public class SignUpActivity extends AppCompatActivity {
     private Button btn_signUp;
     private TextView txt_backToSignIn;
+    private TextInputLayout layout_firstName;
+    private TextInputLayout layout_lastName;
+    private TextInputLayout layout_email;
+    private TextInputLayout layout_password;
+    private TextInputLayout layout_confirmPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        btn_signUp = findViewById(R.id.btn_SignUp);//Sign up button
-        txt_backToSignIn = findViewById(R.id.SignInLink);
+        btn_signUp = findViewById(R.id.btn_signUp);//Sign up button
+        txt_backToSignIn = findViewById(R.id.sighIn_link);
         btn_signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,17 +49,59 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-    public void sign_up(){
+    public void sign_up() {
 
         //DO SIGN UP VALIDATION HERE
 
-        Intent intent=new Intent(this,HomeActivity.class);
+        //password validations
+        TextInputLayout layoutPassword = findViewById(R.id.layout_password);
+        TextInputEditText TextPassword = findViewById(R.id.password);
+
+        TextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String Password = s.toString();
+
+                if (Password.length() >= 8) {
+                    Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+                    Matcher matcher = pattern.matcher(Password);
+                    boolean isPwdContainSpeChar = matcher.find();
+                    if (isPwdContainSpeChar) {
+                        layoutPassword.setHelperText("Strong password");
+                        layoutPassword.setError("");
+
+                    } else {
+                        layoutPassword.setHelperText("");
+                        layoutPassword.setError("Password should contain at 1 special character");
+                    }
+
+
+                } else {
+                    layoutPassword.setHelperText("");
+                    layoutPassword.setError("Enter minimum 8 characters");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 
-    public void goToSignInPage(){
-
-        Intent intent=new Intent(this,LoginActivity.class);
+    public void goToSignInPage() {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
 }
