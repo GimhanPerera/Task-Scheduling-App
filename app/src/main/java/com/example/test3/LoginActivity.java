4f6456d215304ca2e366d6f0fc3efc8af605da5c
Bimindu <aberath-im20004@stu.kln.ac.lk>
2023-11-04 17:14:03 +0530

    new password.

--- app/src/main/java/com/example/test3/LoginActivity.java
+++ app/src/main/java/com/example/test3/LoginActivity.java
@@ -12,14 +12,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.test3.Model.User;
@@ -49,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btn_logIn = findViewById(R.id.btn_confirm);
+        btn_logIn = findViewById(R.id.btn_change);
         txt_signUp = findViewById(R.id.txt_signup);
         txt_email = findViewById(R.id.email);
         txt_password = findViewById(R.id.password);
--- app/src/main/java/com/example/test3/Model/User.java
+++ app/src/main/java/com/example/test3/Model/User.java
@@ -39,4 +39,7 @@ public class User {
     }
 
 
+    public int getId() {
+        return id;
+    }
 }
