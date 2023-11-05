891c08f54fb2489d4e715d5ccb1c278381c8bff0
Tharadias7 <111554482+Tharadias7@users.noreply.github.com>
2023-11-05 22:50:17 +0530

    Testing

--- app/src/main/java/com/example/test3/NewPasswordActivity.java
+++ app/src/main/java/com/example/test3/NewPasswordActivity.java
@@ -2,13 +2,29 @@ package com.example.test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class NewPasswordActivity extends AppCompatActivity {
+
+    private ImageButton back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
+
+        back_btn = findViewById(R.id.Back);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(NewPasswordActivity.this, HomeActivity.class);
+                startActivity(intent);}
+
+        });
+
     }
 }
